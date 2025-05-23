5999252669ba6e823055bfda5c5c338a47996d34
Mayank jha <mayankjha448@gmail.com>
2025-05-23 09:04:51 +0530

    Fix port binding and missing image

--- app.py
+++ app.py
@@ -168,6 +168,10 @@ def home():
     workout=wrk_list
 )
 
+import os
+
+
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
